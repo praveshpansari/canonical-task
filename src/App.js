5895302dc3e3ec04ab3947a6c00e7f01e3dd06da
praveshpansari <praveshpansari@gmail.com>
2023-08-19 15:54:02 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import Post from "./components/post/Post";
+import "./style.scss";
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://people.canonical.com/~anthonydillon/wp-json/wp/v2/posts.json"
+    )
+      .then((res) => res.json())
+      .then((res) => setPosts(res));
+  }, []);
+
+  return (
+    <main>
+      <div className="row u-sv3">
+        <h1>Canonical Task</h1>
+        <em>&mdash; by Pravesh Pansari</em>
+      </div>
+      <div className="row u-equal-height u-clearfix">
+        {posts.map((post, index) => {
+          return (
+            <Post
+              key={index}
+              title={post.title.rendered}
+              image={post.featured_media}
+              author={post._embedded.author[0].name}
+              date={post.date}
+              link={post.link}
+              authorLink={post._embedded.author[0].link}
+              topic={post._embedded["wp:term"][2][0]?.name}
+              category={post._embedded["wp:term"][0][0]?.name}
+            />
+          );
+        })}
+      </div>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/post/Post.js
@@ -0,0 +1,41 @@
+import styles from "./Post.module.css";
+
+const Post = ({
+  image,
+  link,
+  title,
+  authorLink,
+  author,
+  date,
+  topic,
+  category,
+}) => {
+  const publishedDate = new Date(date).toLocaleDateString("en-GB", {
+    year: "numeric",
+    month: "long",
+    date: "numeric",
+  });
+
+  return (
+    <>
+      <div className="col-4">
+        <div className="p-card shadowed">
+          <div class="p-card__content">
+            <p className={styles.heading}>{topic || "cloud and server"}</p>
+            <hr className={styles.dotted} />
+            <img src={image} alt="" className="p-card__image" />
+            <h4 className={styles.expandable_head}>
+              <a href={link}>{title}</a>
+            </h4>
+            <p className="p-heading--6">
+              By <a href={authorLink}>{author}</a> on {publishedDate}
+            </p>
+            <hr className={styles.dotted} />
+            <p>{category || "Article"}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Post;
